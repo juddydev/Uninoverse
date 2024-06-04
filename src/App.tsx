e223150c8c6d15732b6d1178f5e642d6d6a834f8
juddydev <artoh1124@gmail.com>
2024-06-04 22:55:32 +0900

    Update: App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,41 @@
-// import * as THREE from 'three';
-import { Suspense } from 'react';
+import * as THREE from 'three';
+import { Suspense, useRef } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, useGLTF } from '@react-three/drei';
 import './App.css'
 
-// THREE.ColorManagement.legacyMode = false;
-
 function Model({ modelUrl }) {
-  // const group = useRef();
-  const { scene } = useGLTF(modelUrl);
-  // const texture = useLoader(THREE.TextureLoader, textureUrl);
-
-  // useEffect(() => {
-  //   texture.encoding = THREE.sRGBEncoding;
-    
-  //   Object.values(materials).forEach((material) => {
-  //     if (material.map) {
-  //       material.map = texture;
-  //       material.map.encoding = THREE.sRGBEncoding;
-  //       material.color = new THREE.Color(materialColor);
-  //       material.needsUpdate = true;
-  //     }
-  //   });
-  // }, [materials, texture, materialColor]);
-
-  return <primitive object={scene} />;
+  const group = useRef();
+  const { nodes, materials } = useGLTF(modelUrl);
+  console.log(nodes)
+  console.log(materials)
+  return (
+    <group ref={group} {...props} dispose={null}>
+      <mesh castShadow receiveShadow geometry={nodes.shoe.geometry} material={materials.laces} />
+      <mesh castShadow receiveShadow geometry={nodes.shoe_1.geometry} material={materials.mesh} />
+      <mesh castShadow receiveShadow geometry={nodes.shoe_2.geometry} material={materials.caps} />
+      <mesh castShadow receiveShadow geometry={nodes.shoe_3.geometry} material={materials.inner} />
+      <mesh castShadow receiveShadow geometry={nodes.shoe_4.geometry} material={materials.sole} />
+      <mesh castShadow receiveShadow geometry={nodes.shoe_5.geometry} material={materials.stripes} />
+      <mesh castShadow receiveShadow geometry={nodes.shoe_6.geometry} material={materials.band} />
+      <mesh castShadow receiveShadow geometry={nodes.shoe_7.geometry} material={materials.patch} />
+    </group>
+  )
 }
 
 function App() {
   return (
-    <Canvas camera={{ position: [0.8, 0, 0.1], fov: 25 }}>
-      <ambientLight  />
+    <Canvas camera={{ position: [0.8, 0, 0.1], fov: 25 }} shadows>
+      <ambientLight />
       <pointLight position={[10, 10, 10]} />
-      {/* <spotLight /> */}
-      {/* <hemisphereLight skyColor={0xffffff} groundColor={0x444444} intensity={0.6} position={[0, 20, 0]} /> */}
+      <spotLight />
       <directionalLight intensity={Math.PI * 1} position={[10, 30, 5]} />
       <directionalLight intensity={Math.PI * 0.5} position={[-10, 5, 5]} />
       <directionalLight intensity={Math.PI * 1} position={[10, -25, 10]} />
       <directionalLight intensity={Math.PI * 1} position={[0, -10, 10]} />
       <directionalLight intensity={Math.PI * 0.5} position={[10, 25, -10]} />
       <Suspense fallback={null}>
-        <Model modelUrl='scene.gltf' />
+        <Model modelUrl='shoe.glb' />
       </Suspense>
       <OrbitControls />
     </Canvas>
@@ -48,6 +43,3 @@ function App() {
 }
 
 export default App;
-
-
-
